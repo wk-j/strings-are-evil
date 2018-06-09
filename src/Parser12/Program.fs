752efd143fe0af60d58098ae0ae1262cca34d775
wk-j <somnuk.wk@gmail.com>
2018-06-09 13:28:04 +0700

    12

---
+++ src/Parser12/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Text
+
+
+let parseLine(line: StringBuilder) =
+    if line.[0] = 'M' && line.[1] = 'N' && line.[2] = 'O' then
+        ()
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
